f3ca42f518c4a94f159b7f04b1c41371a1c5056e
joffreyj <jojoseph@ualr.edu>
2017-08-03 13:51:11 -0500

    testing include
    testing include

---
+++ js/EconomyIncomePersonal.js
@@ -0,0 +1,9 @@
+var tab3Initialized=false;
+var tab2Initialized=false;
+var tab1Initialized=true;
+
+$(document).ready(function () {
+	
+	$("#chart").load("\IEA_Widgets\pcpi.html");
+	calcite.bus.emit('accordion:toggle',{node: document.getElementById('a2')});
+});
